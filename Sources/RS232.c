fb70669145cab28ef9854f4e7be07ba42f5ae0e0
DESKTOP-7EVIT25\mario <mario.vonfluee@stud.hslu.ch>
2018-03-04 12:23:29 +0100

    Add a Struct for the status to send

--- Sources/RS232.c
+++ Sources/RS232.c
@@ -11,46 +11,112 @@
 #include <stdio.h>
 
 static UART_Desc deviceData;
+static Status_t Status_buffer[STATUS_BUFFER_SIZE];
+static uint8_t CoBuf_inIdx; /* input index */
+static uint8_t CoBuf_outIdx; /* output index */
+static uint8_t CoBuf_inSize; /* size data in buffer */
 
 static void SendChar(char ch, UART_Desc *desc) {
-  desc->isSent = FALSE;  /* this will be set to 1 once the block has been sent */
-  while(AS1_SendBlock(desc->handle, (LDD_TData*)&ch, 1)!=ERR_OK) {} /* Send char */
-  while(!desc->isSent) {} /* wait until we get the green flag from the TX interrupt */
+	desc->isSent = FALSE; /* this will be set to 1 once the block has been sent */
+	while (AS1_SendBlock(desc->handle, (LDD_TData*) &ch, 1) != ERR_OK) {
+	} /* Send char */
+	while (!desc->isSent) {
+	} /* wait until we get the green flag from the TX interrupt */
 }
 
-static void SendString(const unsigned char *str,  UART_Desc *desc) {
-  while(*str!='\0') {
-    SendChar(*str++, desc);
-  }
+static void SendString(const unsigned char *str, UART_Desc *desc) {
+	while (*str != '\0') {
+		SendChar(*str++, desc);
+	}
 }
 
- void RS232Init(void) {
-  /* initialize struct fields */
-  deviceData.handle = AS1_Init(&deviceData);
-  deviceData.isSent = FALSE;
-  deviceData.rxChar = '\0';
-  deviceData.rxPutFct = RxBuf_Put;
-  /* set up to receive RX into input buffer */
-  RxBuf_Init(); /* initialize RX buffer */
-  /* Set up ReceiveBlock() with a single byte buffer. We will be called in OnBlockReceived() event. */
-  while(AS1_ReceiveBlock(deviceData.handle, (LDD_TData *)&deviceData.rxChar, sizeof(deviceData.rxChar))!=ERR_OK) {} /* initial kick off for receiving data */
+/*
+ ** ===================================================================
+ **     Method      :  Status_bufferPut (Status_t temp)
+ **     Description :
+ **         Puts a new element into the buffer
+ **     Parameters  :
+ **         NAME            - DESCRIPTION
+ **
+ **
+ **
+ ** ===================================================================
+ */
+
+void Status_bufferPut(Status_t temp) {
+	if (CoBuf_inSize < STATUS_BUFFER_SIZE) {
+		Status_buffer[CoBuf_inIdx] = temp;
+		CoBuf_inSize++;
+		CoBuf_inIdx++;
+	}
+	if (CoBuf_inSize == STATUS_BUFFER_SIZE) {
+		CoBuf_inIdx = 0;
+	}
+}
+
+/*
+ ** ===================================================================
+ **     Method      :  Status_bufferPut (Status_t temp)
+ **     Description :
+ **         Removes an element into the buffer
+ **     Parameters  :
+ **         NAME            - DESCRIPTION
+ **
+ **
+ **
+ ** ===================================================================
+ */
+
+Status_t Status_bufferPull(void) {
+	Status_t temp;
+	if (CoBuf_inSize > 0) {
+		temp = Status_buffer[CoBuf_outIdx];
+		CoBuf_inSize--;
+		CoBuf_outIdx++;
+		if (CoBuf_outIdx == STATUS_BUFFER_SIZE) {
+			CoBuf_outIdx = 0;
+		}
+	} else {
+		return;
+	}
+	return temp;
+}
+
+void RS232Init(void) {
+	CoBuf_inIdx = 0;
+	CoBuf_outIdx = 0;
+	CoBuf_inSize = 0;
+	/* initialize struct fields */
+	deviceData.handle = AS1_Init(&deviceData);
+	deviceData.isSent = FALSE;
+	deviceData.rxChar = '\0';
+	deviceData.rxPutFct = RxBuf_Put;
+	/* set up to receive RX into input buffer */
+	RxBuf_Init(); /* initialize RX buffer */
+	/* Set up ReceiveBlock() with a single byte buffer. We will be called in OnBlockReceived() event. */
+	while (AS1_ReceiveBlock(deviceData.handle, (LDD_TData *) &deviceData.rxChar,
+			sizeof(deviceData.rxChar)) != ERR_OK) {
+	} /* initial kick off for receiving data */
+}
+
+char Convert_uint16ToChar(uint16_t speed) {
+
 }
 
 void Send_Satus(char test) {
-  int i;
-  SendChar(test, &deviceData);
-  //for(;;) {
-    //if (RxBuf_NofElements()!=0) {
-      //SendString((unsigned char*)"echo: ", &deviceData);
-     // while (RxBuf_NofElements()!=0) {
-       // unsigned char ch;
+	int i;
+	SendChar(test, &deviceData);
+	//for(;;) {
+	//if (RxBuf_NofElements()!=0) {
+	//SendString((unsigned char*)"echo: ", &deviceData);
+	// while (RxBuf_NofElements()!=0) {
+	// unsigned char ch;
 
-        //(void)RxBuf_Get(&ch);
-        //SendChar(ch, &deviceData);
-      //}
-      //SendString((unsigned char*)"\r\n", &deviceData);
-    //}
- // }
+	//(void)RxBuf_Get(&ch);
+	//SendChar(ch, &deviceData);
+	//}
+	//SendString((unsigned char*)"\r\n", &deviceData);
+	//}
+	// }
 }
-
 
--- Sources/RS232.h
+++ Sources/RS232.h
@@ -11,12 +11,23 @@
 #include "PE_Types.h"
 #include "PE_LDD.h"
 
+#ifndef STATUS_BUFFER_SIZE
+  #define STATUS_BUFFER_SIZE         64   /* number of elements in the buffer */
+#endif
+
 typedef struct {
   LDD_TDeviceData *handle; /* LDD device handle */
   volatile uint8_t isSent; /* this will be set to 1 once the block has been sent */
   uint8_t rxChar; /* single character buffer for receiving chars */
   uint8_t (*rxPutFct)(uint8_t); /* callback to put received character into buffer */
 } UART_Desc;
+
+typedef struct {
+	int16_t drivePosition;	/* Position of the drive motor in mm/s */
+	int16_t winchPosition; /* Position of the winch motor in mm/s */
+	uint8_t controlStatus;	/*Status signal Bit[0] 0/1 Electromanet off/on, Bit[1] 0/1 drive motor no Reset/Reset, Bit[2] 0/1 winch motor no Reset/Reset*/
+} Status_t;
+
 
 void Send_Satus(char test);
 void RS232Init(void);
